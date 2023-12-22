6c89ff0c6d3936e4ad893bf7a9993f621fe0e124
naro_Kim <51940808+naro-Kim@users.noreply.github.com>
2023-12-22 19:47:17 +0900

    chore: initalize package setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			animation: {
+				reveal: 'reveal 1.5s cubic-bezier(0.77, 0, 0.175, 1) 0.5s',
+			},
+			keyframes: {
+				reveal: {
+					'0%': {
+						transform: 'translate(0, 100%)',
+					},
+					'100%': {
+						transform: 'translate(0, 0)',
+					},
+				},
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
