988730ab6c7e09346cc63276c4b80ea9b0911384
naro_Kim <gracias9022@gmail.com>
2023-12-24 01:50:26 +0900

    feat: update snow asset, fix the scene

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-// import dynamic from 'next/dynamic';
-
-import { WindowCanvas } from '@/components/WindowCanvas';
-
+import dynamic from 'next/dynamic';
 export default function Home() {
-	// const WindowCanvas = dynamic(() => import('@/components/'), {
-	// 	ssr: false,
-	// 	loading: () => <p>Loading...</p>,
-	// });
+	const WindowCanvas = dynamic(() => import('@/components/'), {
+		ssr: false,
+		loading: () => <div className='w-full text-center'><p>Loading...</p></div>,
+	});
 
 	return (
 		<main className="grid min-h-content grid-flow-row gap-24">
-			<WindowCanvas />
-			{/* <WindowScene /> */}
+			<WindowCanvas /> 
 		</main>
 	);
 }
