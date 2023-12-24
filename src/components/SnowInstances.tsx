5b2017c0f38fabf1d7e7b983b78efeaa0b65789a
naro_Kim <gracias9022@gmail.com>
2023-12-24 19:21:11 +0900

    feat: implement z,x axis movement

--- src/components/SnowInstances.tsx
+++ src/components/SnowInstances.tsx
@@ -1,78 +1,101 @@
-import * as THREE from 'three';
-import { useMemo, useRef } from 'react';
-import { useFrame, useLoader, useThree } from '@react-three/fiber';
-// import Snowflake from './Snowflake';
+import * as THREE from "three";
+import { useMemo, useRef } from "react";
+import { useFrame, useLoader, useThree } from "@react-three/fiber";
 
-const SnowInstances = ({ count = 1000 }) => {
-	const particles = useRef<THREE.Points>(null!);
-	const positionRef = useRef<THREE.BufferAttribute>(null!);
-	const velocityRef = useRef<THREE.BufferAttribute>(null!);
-	const [minRange, maxRange] = useMemo(() => [-2, 20], []);
+const SnowInstances = ({ count = 10000 }) => {
+  const particles = useRef<THREE.Points>(null!);
+  const positionRef = useRef<THREE.BufferAttribute>(null!);
+  const velocityRef = useRef<THREE.BufferAttribute>(null!);
+  const [minRange, maxRange] = useMemo(() => [-2, 20], []);
 
-	const points = useMemo(() => {
-		const p = new Array(count).fill(0).map((v) => (0.5 - Math.random()) * maxRange);
-		return new THREE.BufferAttribute(new Float32Array(p), 3);
-	}, [count]);
+  const points = useMemo(() => {
+    const p = new Array(count)
+      .fill(0)
+      .map((v) => (0.5 - Math.random()) * maxRange);
+    return new THREE.BufferAttribute(new Float32Array(p), 3);
+  }, [count]);
 
-	const velocities = useMemo(() => {
-		const v = new Array(count * 3).fill(0).map(() => (Math.random() - 0.5) * 0.1);
-		return new THREE.BufferAttribute(new Float32Array(v), 3);
-	}, [count]);
+  const velocities = useMemo(() => {
+    const v = new Array(count * 3)
+      .fill(0)
+      .map(() => (Math.random() - 0.5) * 0.1);
+    return new THREE.BufferAttribute(new Float32Array(v), 3);
+  }, [count]);
 
-	const flakeMaterial = useMemo(() => {
-		const snowflakeMap = useLoader(THREE.TextureLoader, '/assets/snowflake.webp');
-		const mat = {
-			size: 0.1,
-			color: 0xffffff,
-			vertexColors: false,
-			map: snowflakeMap,
-			transparent: true,
-			// opacity: 0.5,
-			fog: true,
-			depthWrite: false,
-		};
-		return mat;
-	}, []);
+  const flakeMaterial = useMemo(() => {
+    const snowflakeMap = useLoader(
+      THREE.TextureLoader,
+      "/assets/snowflake.webp"
+    );
+    const mat = {
+      size: 0.1,
+      color: 0xffffff,
+      vertexColors: false,
+      map: snowflakeMap,
+      transparent: true,
+      // opacity: 0.5,
+      fog: true,
+      depthWrite: false,
+    };
+    return mat;
+  }, []);
 
+  useFrame((_, dt) => {
+    console.log(positionRef);
+    const posArr = positionRef.current.array;
+    const velArr = velocityRef.current.array;
 
-	useFrame((_, dt) => {
-		const posArr = positionRef.current.array;
-		const velArr = velocityRef.current.array;
+    for (let i = 0; i < posArr.length; i += 3) {
+      const x = i;
+      const y = i + 1;
+      const z = i + 2;
 
-		for (let i = 0; i < posArr.length; i += 3) {
-			const x = i;
-			const y = i + 1;
-			const z = i + 2;
+      const velX = Math.sin(dt * 0.1 * (i < 3 ? i + 1 : -(i + 1))) * 0.01;
+      const velY = Math.cos(dt * 0.01 * velArr[y]) * 0.03;
+      const velZ = Math.cos(dt * 0.01 * (i < 3 ? i + 1 : -(i + 1))) * 0.01;
 
-			const velX = Math.sin(dt * 0.001 * velArr[x]) * 0.1;
-			const velY = Math.cos(dt * 0.0015 * velArr[y]) * 0.02;
-			const velZ = Math.sin(dt * 0.02 * velArr[z]) * Math.PI * 0.1;
+      // positions[i] += Math.sin(clock.elapsedTime * 0.1) * 0.1; // Adjust the speed as needed
+      // positions[i + 2] += Math.cos(clock.elapsedTime * 0.1) * 0.1; // Adjust the speed as needed
 
-			posArr[x] += velX;
-			posArr[y] -= velY;
-			posArr[z] += velZ;
+      posArr[x] += velX;
+      posArr[y] -= velY;
+      posArr[z] += velZ;
 
-			// accumulation snow
-			if (posArr[y] < minRange) {
-				posArr[y] = maxRange - (i * 0.01);
-			} 
-		}
+      // accumulation snow
+      if (posArr[y] < minRange) {
+        posArr[y] = maxRange - i * 0.01;
+      }
+      if (posArr[z] > maxRange) {
+        posArr[z] = minRange - i * 0.01;
+      }
+      if (posArr[x] > maxRange) {
+        posArr[x] = minRange - i * 0.01;
+      }
+    }
 
-		positionRef.current.needsUpdate = true;
-		velocityRef.current.needsUpdate = true;
-	});
+    positionRef.current.needsUpdate = true;
+    velocityRef.current.needsUpdate = true;
+  });
 
-	return (
-		<group position={[0, minRange, 0]}>
-			<points ref={particles}>
-				<bufferGeometry>
-					<bufferAttribute ref={positionRef} attach="attributes-position" {...points} />
-					<bufferAttribute ref={velocityRef} attach="attributes-velocity" {...velocities} />
-				</bufferGeometry>
-				<pointsMaterial {...flakeMaterial} sizeAttenuation={true} />
-			</points>
-		</group>
-	);
+  return (
+    <group position={[0, minRange, 0]}>
+      <points ref={particles}>
+        <bufferGeometry>
+          <bufferAttribute
+            ref={positionRef}
+            attach="attributes-position"
+            {...points}
+          />
+          <bufferAttribute
+            ref={velocityRef}
+            attach="attributes-velocity"
+            {...velocities}
+          />
+        </bufferGeometry>
+        <pointsMaterial {...flakeMaterial} sizeAttenuation={true} />
+      </points>
+    </group>
+  );
 };
 
 export default SnowInstances;
--- src/components/WindowCanvas.tsx
+++ src/components/WindowCanvas.tsx
@@ -1,31 +1,31 @@
-'use client';
+"use client";
 
-import { Canvas } from '@react-three/fiber';
-import { useMemo } from 'react';
-import { Euler, Vector3 } from 'three';
-import { PerspectiveCamera, Environment } from '@react-three/drei';
-import { Bloom, DepthOfField, EffectComposer, HueSaturation, Vignette } from '@react-three/postprocessing'
-import SnowInstances from '@/components/SnowInstances';
-import SnowScene from '@/components/SnowScene';
-
+import { Canvas } from "@react-three/fiber";
+import { useMemo } from "react";
+import { Euler, Vector3 } from "three";
+import {
+  PerspectiveCamera,
+  Environment,
+  OrbitControls,
+} from "@react-three/drei";
+import SnowInstances from "@/components/SnowInstances";
+import SnowScene from "@/components/SnowScene";
+import CameraRig from "@/components/CameraRig";
 export const WindowCanvas = () => {
-	const aspect = window.innerWidth / window.innerHeight;
-	const dist = useMemo(() => aspect / (2 * Math.tan((50 * Math.PI) / 360)), []);
-	const pos = useMemo(() => new Vector3(0, 1.2, 2 + dist), [])
-	return (
-		<Canvas className={'w-full h-full'}>
-			<EffectComposer>
-				<DepthOfField focusDistance={0} focalLength={aspect / Math.PI} bokehScale={1} height={480} />
-				<Bloom luminanceThreshold={0.8} luminanceSmoothing={0.9} height={240} />
-				<Vignette eskil={false} offset={0.2} darkness={1.1} />
-				<HueSaturation saturation={0.1} />
-			</EffectComposer>
-			<Environment preset='forest' />
-			<PerspectiveCamera makeDefault position={pos} aspect={aspect} />
-			<ambientLight intensity={2} position={[100, 5, 1]} />
-			<hemisphereLight scale={10} position={[0, 0, 0]} ></hemisphereLight> 
-			<SnowInstances />
-			<SnowScene rotation={new Euler(0, Math.PI, 0)} />
-		</Canvas>
-	);
+  const aspect = window.innerWidth / window.innerHeight;
+  const dist = useMemo(() => aspect / (2 * Math.tan((50 * Math.PI) / 360)), []);
+  const pos = useMemo(() => new Vector3(0, 1.2, 2 + dist), []);
+
+  return (
+    <Canvas className={"w-full h-full"}>
+      <Environment preset="forest" />
+      <PerspectiveCamera makeDefault position={pos} aspect={aspect} />
+      <ambientLight intensity={2} position={[100, 5, 1]} />
+      <hemisphereLight scale={10} position={[0, 0, 0]} />
+      <SnowInstances />
+      <SnowScene rotation={new Euler(0, Math.PI, 0)} />
+      {/* <CameraRig /> */}
+      <OrbitControls />
+    </Canvas>
+  );
 };
