61642e42db22e844dc9e7eb000c40723bfa05402
naro_Kim <gracias9022@gmail.com>
2023-12-27 00:33:56 +0900

    chore: add file-loader package for music

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: `/_next/static/sounds/`,
+          outputPath: `${options.isServer ? "../" : ""}static/sounds/`,
+        },
+      },
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
