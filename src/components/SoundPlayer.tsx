bbcc4ac0fdf03beb8675632879d7d4e87cc2bfb8
naro_Kim <gracias9022@gmail.com>
2023-12-29 12:27:47 +0900

    fix: sound import path

--- src/components/SoundPlayer.tsx
+++ src/components/SoundPlayer.tsx
@@ -1,5 +1,13 @@
-import jollyChristmas from '../../public/assets/jollyChristmas.mp3'
+// import jollyChristmas from '../../public/assets/jollyChristmas.mp3'
 
 export const SoundPlayer = () => {
-  return <audio className='absolute w-min-content left-2 top-2 z-10 opacity-20' src={jollyChristmas} autoPlay controls loop />
+  return (
+    <audio
+      className="absolute w-min-content left-2 top-2 z-10 opacity-20"
+      src={"/assets/jollyChristmas.mp3"}
+      autoPlay
+      controls
+      loop
+    />
+  );
 };
