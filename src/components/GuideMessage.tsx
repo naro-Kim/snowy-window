4b82b7ef6b457ef0e31f3e602113a146f6a09343
naro_Kim <gracias9022@gmail.com>
2023-12-31 22:12:18 +0900

    feat: 방명록 기능 일단 완료

--- src/components/GuideMessage.tsx
+++ src/components/GuideMessage.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useSceneContext } from "@/context/SceneContext";
+import Link from "next/link";
 import { useCallback, useRef, } from "react";
 
 export const GuideMessage = () => {
@@ -22,9 +23,13 @@ export const GuideMessage = () => {
     <>
       <dialog open={zoom} className={`w-full sm:w-1/2 max-w-7xl rounded-xl bg-[rgba(0,0,0,0.5)] z-10 absolute bottom-1/4 left-1/2 -translate-x-1/2 translate-y-1/2 duration-500`}>
         <div className={"text-white p-4 sm:p-8 grid grid-flow-rows gap-2"}>
-          <h1 className={"font-semibold text-md sm:text-lg"}>Leave a Comment!</h1>
+          <div className="grid grid-flow-col mb-2">
+            <h1 className={"font-semibold text-md sm:text-lg"}>Leave a Comment!</h1>
+            <button className="justify-self-end text-xs max-w-[160px] bg-gray-500/25 p-2 rounded-lg"><Link href={`https://github.com/naro-Kim/snowy-window`}>개발 깃허브 바로가기</Link></button>
+          </div>
           <span className={"font-light leading-relaxed text-pretty text-xs sm:text-sm"}>
-            <p>Share your thoughts about this project. Click "Submit" when you're done or "Cancel" to close the dialog.</p>
+            {/* <p>Share your thoughts about this project. Click "Submit" when you're done or "Cancel" to close the dialog.</p> */}
+            <p>반가워요! 새해를 맞이하는 마음으로, 일주일 간 개발한 react-three-fiber 프로젝트입니다. 프로젝트를 응원하는 메세지를 남겨주시면 큰 힘이 됩니다!</p>
           </span>
           <form onSubmit={handleSubmit} className="grid gap-4 py-4">
             <div className="grid grid-cols-4 items-center gap-4">
